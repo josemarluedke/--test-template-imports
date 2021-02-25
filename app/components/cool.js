992effe63f948ad5a349b9b2725762f446e7ff87
Josemar Luedke <josemarluedke@gmail.com>
2021-02-25 14:11:36 -0800

    Simplify

--- app/components/cool.js
+++ app/components/cool.js
@@ -1,11 +1,6 @@
 import Component from '@glimmer/component';
 import { hbs } from 'ember-template-imports';
-import { helper } from '@ember/component/helper';
 import { Button } from './other/bla';
-
-const bla = helper(() => {
-  return 'bla';
-});
 
 export default class Hello extends Component {
   name = 'world';
@@ -13,6 +8,6 @@ export default class Hello extends Component {
   static template = hbs`
     <span>Hello, {{this.name}}!</span>
 
-    <Button class="button">{{bla}}</Button>
+    <Button class="button">Cool</Button>
   `;
 }
